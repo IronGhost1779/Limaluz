114fdd50c037ad74e023cc132f58d5fe386f464c
Gustavo <gaberdugov@unal.edu.co>
2025-07-04 19:53:40 -0500

    Commit PP actualizado mas camara

--- Quickstart-master/TeamCode/src/main/java/pedroPathing/limaluz/intento.java
+++ Quickstart-master/TeamCode/src/main/java/pedroPathing/limaluz/intento.java
@@ -9,13 +9,11 @@ import static com.pedropathing.follower.FollowerConstants.rightFrontMotorName;
 import static com.pedropathing.follower.FollowerConstants.rightRearMotorDirection;
 import static com.pedropathing.follower.FollowerConstants.rightRearMotorName;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.pedropathing.localization.PoseUpdater;
 import com.pedropathing.util.Constants;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
@@ -28,8 +26,8 @@ import java.util.List;
 import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
 
-
-public class Teleop extends LinearOpMode {
+@TeleOp
+public class Intento extends LinearOpMode {
     private DcMotorEx leftFront;
     private DcMotorEx leftRear;
     private DcMotorEx rightFront;
@@ -38,8 +36,15 @@ public class Teleop extends LinearOpMode {
 
     private Limelight3A limelight;
 
+    @Override
 
-    public void init() {
+
+
+
+
+
+    public void runOpMode() throws InterruptedException
+    {
         Constants.setConstants(FConstants.class, LConstants.class);
 
         leftFront = hardwareMap.get(DcMotorEx.class, leftFrontMotorName);
@@ -62,24 +67,18 @@ public class Teleop extends LinearOpMode {
         for (DcMotorEx motor : motors) {
             motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         }
-    }
 
-
-
-
-    @Override
-    public void runOpMode() throws InterruptedException
-    {
-        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight = hardwareMap.get(Limelight3A.class, "limaluz");
 
         telemetry.setMsTransmissionInterval(11);
 
-        limelight.pipelineSwitch(0);
+        limelight.pipelineSwitch(7);
 
-        /*
-         * Starts polling for data.
-         */
+        double error_ll;
+
         limelight.start();
+
+        waitForStart();
 
         while (opModeIsActive()) {
             LLResult result = limelight.getLatestResult();
@@ -89,10 +88,18 @@ public class Teleop extends LinearOpMode {
                     telemetry.addData("tx", result.getTx());
                     telemetry.addData("ty", result.getTy());
                     telemetry.addData("Botpose", botpose.toString());
+                    telemetry.update();
                 }
+                error_ll = result.getTx();
+            }else{
+                error_ll = 0;
             }
 
-            double error_ll = result.getTx();
+
+
+
+
+
             double kp = 0.02;
             double control = kp*error_ll;
 
